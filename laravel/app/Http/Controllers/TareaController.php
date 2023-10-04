fe07d40faa8367b7fbd5b1bcf1699789065aa9f8
Ignacio Tejera <itejera01@gmail.com>
2023-10-04 13:56:57 -0300

    Agregado de Modelo de Tarea y función AgregarTarea junto a su acompañante crearTarea

--- laravel/app/Http/Controllers/TareaController.php
+++ laravel/app/Http/Controllers/TareaController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Tarea;
 
 class TareaController extends Controller
 {
-    //
+
+    public function AgregarTarea(Request $request)
+    {
+        $Tarea = new Tarea();
+        $Tarea = crearTarea($Tarea, $request);
+        $Tarea->save();
+    }
+
+    private function crearTarea($Tarea, $request)
+    {
+        $Tarea->Titulo = $request['titulo'];
+        $Tarea->Contenido = $request['contenido'];
+        $Tarea->Estado = $request['estado'];
+        $Tarea->Autor = $request['autor'];
+        return $Tarea;
+    }
 }

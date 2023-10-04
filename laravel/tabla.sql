c3a4faf651fec19b41f75284402cb14093e6fccd
Ignacio Tejera <itejera01@gmail.com>
2023-10-04 13:42:35 -0300

    Agregado de Controlador y archivo de creación de tabla

---
+++ laravel/tabla.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Tarea(
+    Id serial,
+    Titulo varchar(255),
+    Contenido varchar(255),
+    Estado varchar(255),
+    autor varchar(255),
+    created_at timestamp,
+    updated_at timestamp,
+    deleted_at timestamp
+)
